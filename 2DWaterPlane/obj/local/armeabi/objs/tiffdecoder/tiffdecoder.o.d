83ed16b64b49b3e2dd55fec502c17fccb3986944
Steven Ellis <steven.ellis.d@gmail.com>
2014-02-19 18:23:59 -0500

    recompiled gdal with thread support

--- 2DWaterPlane/obj/local/armeabi/objs/tiffdecoder/tiffdecoder.o.d
+++ 2DWaterPlane/obj/local/armeabi/objs/tiffdecoder/tiffdecoder.o.d
@@ -1,86 +1,89 @@
-/home/steve/Android/git/2dwaterplane/2DWaterPlane/obj/local/armeabi/objs/tiffdecoder/tiffdecoder.o: \
- /home/steve/Android/git/2dwaterplane/2DWaterPlane/jni/tiffdecoder.c \
- /home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/jni.h \
- /home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/stdio.h \
- /home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
- /home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
- /home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/android/api-level.h \
- /home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/types.h \
- /home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/stdint.h \
- /home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/_types.h \
- /home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/machine/_types.h \
- /home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/linux/posix_types.h \
- /home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/linux/stddef.h \
- /home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/linux/compiler.h \
- /home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/asm/posix_types.h \
- /home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/asm/types.h \
- /home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/linux/types.h \
- /home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/machine/kernel.h \
- /home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h \
- /home/steve/Android/git/2dwaterplane/2DWaterPlane/jni/tiffio.h \
- /home/steve/Android/git/2dwaterplane/2DWaterPlane/jni/tiff.h \
- /home/steve/Android/git/2dwaterplane/2DWaterPlane/jni/tiffconf.h \
- /home/steve/Android/git/2dwaterplane/2DWaterPlane/jni/tiffvers.h \
- /home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/android/log.h \
- /home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/stdlib.h \
- /home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/string.h \
- /home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/malloc.h \
- /home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/alloca.h \
- /home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/strings.h \
- /home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/memory.h
+/home/steve/AndroidStudioProjects/water-plane-app/2DWaterPlane/obj/local/armeabi/objs/tiffdecoder/tiffdecoder.o: \
+ /home/steve/AndroidStudioProjects/water-plane-app/2DWaterPlane/jni/tiffdecoder.c \
+ /home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/jni.h \
+ /home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/cdefs.h \
+ /home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/android/api-level.h \
+ /home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdio.h \
+ /home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/types.h \
+ /home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdint.h \
+ /home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/_types.h \
+ /home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/_types.h \
+ /home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/_wchar_limits.h \
+ /home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/posix_types.h \
+ /home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/stddef.h \
+ /home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/compiler.h \
+ /home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/posix_types.h \
+ /home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/types.h \
+ /home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/types.h \
+ /home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/kernel.h \
+ /home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h \
+ /home/steve/AndroidStudioProjects/water-plane-app/2DWaterPlane/jni/tiffio.h \
+ /home/steve/AndroidStudioProjects/water-plane-app/2DWaterPlane/jni/tiff.h \
+ /home/steve/AndroidStudioProjects/water-plane-app/2DWaterPlane/jni/tiffconf.h \
+ /home/steve/AndroidStudioProjects/water-plane-app/2DWaterPlane/jni/tiffvers.h \
+ /home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/android/log.h \
+ /home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdlib.h \
+ /home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/string.h \
+ /home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/malloc.h \
+ /home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/alloca.h \
+ /home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/strings.h \
+ /home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/memory.h
 
-/home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/jni.h:
+/home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/jni.h:
 
-/home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/stdio.h:
+/home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/cdefs.h:
 
-/home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
+/home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h:
 
-/home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
+/home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/android/api-level.h:
 
-/home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/android/api-level.h:
+/home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdio.h:
 
-/home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/types.h:
+/home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/types.h:
 
-/home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/stdint.h:
+/home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdint.h:
 
-/home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/_types.h:
+/home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/_types.h:
 
-/home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/machine/_types.h:
+/home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/_types.h:
 
-/home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/linux/posix_types.h:
+/home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/_wchar_limits.h:
 
-/home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/linux/stddef.h:
+/home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/posix_types.h:
 
-/home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/linux/compiler.h:
+/home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/stddef.h:
 
-/home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/asm/posix_types.h:
+/home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/compiler.h:
 
-/home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/asm/types.h:
+/home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/posix_types.h:
 
-/home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/linux/types.h:
+/home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/types.h:
 
-/home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/machine/kernel.h:
+/home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/types.h:
 
-/home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h:
+/home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/kernel.h:
 
-/home/steve/Android/git/2dwaterplane/2DWaterPlane/jni/tiffio.h:
+/home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h:
 
-/home/steve/Android/git/2dwaterplane/2DWaterPlane/jni/tiff.h:
+/home/steve/AndroidStudioProjects/water-plane-app/2DWaterPlane/jni/tiffio.h:
 
-/home/steve/Android/git/2dwaterplane/2DWaterPlane/jni/tiffconf.h:
+/home/steve/AndroidStudioProjects/water-plane-app/2DWaterPlane/jni/tiff.h:
 
-/home/steve/Android/git/2dwaterplane/2DWaterPlane/jni/tiffvers.h:
+/home/steve/AndroidStudioProjects/water-plane-app/2DWaterPlane/jni/tiffconf.h:
 
-/home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/android/log.h:
+/home/steve/AndroidStudioProjects/water-plane-app/2DWaterPlane/jni/tiffvers.h:
 
-/home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/stdlib.h:
+/home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/android/log.h:
 
-/home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/string.h:
+/home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdlib.h:
 
-/home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/malloc.h:
+/home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/string.h:
 
-/home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/alloca.h:
+/home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/malloc.h:
 
-/home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/strings.h:
+/home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/alloca.h:
 
-/home/steve/Android/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/memory.h:
+/home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/strings.h:
+
+/home/steve/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/memory.h:
